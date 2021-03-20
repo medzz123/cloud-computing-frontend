968d81686c22528686eddaffc46b551b723b49eb
Miriam Rodriguez Garcia (ED-A-10) <Miriam.Rodriguez-Garcia@bmw.de>
2021-03-20 21:17:51 +0100

    Fixes background color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,
-  {scrollPositionRestoration: 'enabled'})],
+  {scrollPositionRestoration: 'enabled'})
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
